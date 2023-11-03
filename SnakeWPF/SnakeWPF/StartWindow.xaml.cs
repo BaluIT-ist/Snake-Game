d9dd41f856cf5fa143dac9aa75272b2d7563f0b1
dgadrian05 <dgadrian05@gmail.com>
2023-11-03 13:09:03 +0200

    Implemented Start button Implemented Exit button Added Start Windown

---
+++ SnakeWPF/SnakeWPF/StartWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakeWPF
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow=new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void Menu_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
